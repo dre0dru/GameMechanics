cd8247a3401cdfc038a4ff1d25633a7b899a5361
Dre0Dru <dre0druofficial@gmail.com>
2023-09-10 00:46:44 +0300

    feat: patterns extensions

---
+++ Runtime/Projectiles/Patterns/PatternsExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Dre0Dru.MathExtensions;
+using UnityEngine;
+
+namespace Dre0Dru.Projectiles.Patterns
+{
+    public static class PatternsExtensions
+    {
+        public static (Vector3 direction, Vector3 position) ToDirectionAndPositionY(this PatternOutput patternOutput,
+            Transform transform)
+        {
+            return (patternOutput.ToDirectionY(transform), patternOutput.ToPositionY(transform));
+        }
+
+        public static Vector3 ToDirectionY(this PatternOutput patternOutput, Transform transform)
+        {
+            return VectorExtensions
+                .CalculateDirectionFromAngle2D(patternOutput.Angle + transform.eulerAngles.y)
+                .ToVector3XZ(transform.position.y);
+        }
+
+        public static Vector3 ToPositionY(this PatternOutput patternOutput, Transform transform)
+        {
+            return transform.position + transform.right * patternOutput.Offset.x +
+                   transform.forward * patternOutput.Offset.y;
+        }
+    }
+}
