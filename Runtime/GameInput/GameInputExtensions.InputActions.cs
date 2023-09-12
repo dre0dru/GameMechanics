278987c406b4f04402bd8bd29aa20ad813cc5a76
Dre0Dru <dre0druofficial@gmail.com>
2023-09-12 10:29:13 +0300

    feat: more input button state extensions

--- Runtime/GameInput/GameInputExtensions.InputActions.cs
+++ Runtime/GameInput/GameInputExtensions.InputActions.cs
@@ -1,25 +1,52 @@
-﻿using UnityEngine.InputSystem;
+﻿using System;
+using UnityEngine.InputSystem;
 
 namespace Dre0Dru.GameInput
 {
     public static partial class GameInputExtensions
     {
-        public static void UpdatePressedStateFromContext(this ButtonState buttonState, InputAction.CallbackContext ctx) =>
+        public static void
+            UpdatePressedStateFromContext(this ButtonState buttonState, InputAction.CallbackContext ctx) =>
             buttonState.UpdatePressedState();
 
-        public static void UpdateReleasedStateFromContext(this ButtonState buttonState, InputAction.CallbackContext ctx) =>
+        public static void
+            UpdateReleasedStateFromContext(this ButtonState buttonState, InputAction.CallbackContext ctx) =>
             buttonState.UpdateReleasedState();
 
-        public static void AddButtonState(this InputAction inputAction, ButtonState buttonState)
+        public static void AddButtonState(this InputAction inputAction, ButtonState buttonState,
+            Action<InputAction.CallbackContext> onPerformed = null,
+            Action<InputAction.CallbackContext> onCancelled = null)
         {
             inputAction.performed += buttonState.UpdatePressedStateFromContext;
             inputAction.canceled += buttonState.UpdateReleasedStateFromContext;
+
+            if (onPerformed != null)
+            {
+                inputAction.performed += onPerformed;
+            }
+
+            if (onCancelled != null)
+            {
+                inputAction.canceled += onCancelled;
+            }
         }
 
-        public static void RemoveButtonState(this InputAction inputAction, ButtonState buttonState)
+        public static void RemoveButtonState(this InputAction inputAction, ButtonState buttonState,
+            Action<InputAction.CallbackContext> onPerformed = null,
+            Action<InputAction.CallbackContext> onCancelled = null)
         {
             inputAction.performed -= buttonState.UpdatePressedStateFromContext;
             inputAction.canceled -= buttonState.UpdateReleasedStateFromContext;
+
+            if (onPerformed != null)
+            {
+                inputAction.performed -= onPerformed;
+            }
+
+            if (onCancelled != null)
+            {
+                inputAction.canceled -= onCancelled;
+            }
         }
     }
 }
